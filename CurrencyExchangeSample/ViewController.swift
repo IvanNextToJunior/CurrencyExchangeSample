a405e1e99ea69cdd5df542ced3f808e97b9feae0
IvanNextToJunior <58988299+IvanNextToJunior@users.noreply.github.com>
2025-04-10 16:43:05 +0300

    add toolbar

--- CurrencyExchangeSample/ViewController.swift
+++ CurrencyExchangeSample/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak private var currencySegmentedControl: UISegmentedControl!
     
     @IBOutlet weak private var sumTextField: UITextField!
@@ -17,9 +17,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencySegmentedControl.setTitle("USD", forSegmentAt: 0)
+        currencySegmentedControl.setTitle("EUR", forSegmentAt: 1)
+        createToolBar()
     }
-
-
+    
+    
+    private func createToolBar() {
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //MARK: Change position of UIToolBar: https://www.hackingwithswift.com/forums/swift/toolbar-button-location/5666
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissKeyboard))
+        
+        toolbar.setItems([flexibleSpace, doneButton], animated: true)
+        toolbar.isUserInteractionEnabled = true
+        
+        sumTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
 }
 
